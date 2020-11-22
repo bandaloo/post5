54f557eaa46b0fbdd35007d7e21697ad6b2f1347
bandaloo <bandaloo@icloud.com>
2020-11-21 20:32:34 -0500

    changed version in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "/* post5 0.2.1 | (c) 2020, Cole Granof | MIT License */" > post5.js
+echo "/* post5 0.1.0 | (c) 2020, Cole Granof | MIT License */" > post5.js
 cat p5bundle.js p5extensions.js >> post5.js
